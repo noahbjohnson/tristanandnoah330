db3034d21c5fb3bcff79d071e564dfef9b18e15f
Noah B Johnson <johnno01@luther.edu>
2018-04-10 22:28:18 -0500

    continued work on api handling and request processing

--- scripts/index.js
+++ scripts/index.js
@@ -5,12 +5,16 @@ CS-330
 Luther College- Dr. Brad Miller
 
 Type: JS ECMA 6
+Requires: Jquery
  */
 
+let languageList = ['ermagherd','leetspeak','vulcan'];
+let requestsList = [];
+
 class request{
-    constructor(inputString){
+    constructor(inputString,languageString){
         this.inputString = inputString;
-        this.length = length(this.inputString);
+        this.len = length(this.inputString);
 
 
         // Remove double spaces and trailing/leading spaces from the input string
@@ -19,11 +23,40 @@ class request{
             workingString = workingString.replace("  ", " ")
         }
         workingString = workingString.toLowerCase();
+        workingString = workingString.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 
+        this.cleanString = workingString;
+        this.uriString = encodeURI(this.cleanString);
     }
     toString(){
-        return this.inputString;
+        return this.cleanString;
     }
+}
+
+function funTranslate(language,text='ermahgerd'){
 
 }
+
+function clickon() {
+    let textInput = document.getElementById('inputText').value;
+    let textLanguage = document.getElementById('language').value;
+}
+
+function populateSelect(selectId, sList) {
+    let sel = document.getElementById(selectId);
+    for (let s of sList) {
+        let opt = document.createElement("option");
+        opt.value = s;
+        opt.innerHTML = s;
+        sel.appendChild(opt)
+    }
+}
+
+
+$( document ).ready(function() {
+    populateSelect('language',languageList)
+});
+
+
+
 
