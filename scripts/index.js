d3b2a33f02604a8f0b2fb4b48147bdd055befba2
Noah B Johnson <johnno01@luther.edu>
2018-04-10 21:32:52 -0500

    Began work on class for request handling

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,29 @@
 /*
- 
+Javascript Api Mash-up Project
+Noah Johnson & Tristan Shaffer
+CS-330
+Luther College- Dr. Brad Miller
 
-
-
-
-
-
-
+Type: JS ECMA 6
  */
+
+class request{
+    constructor(inputString){
+        this.inputString = inputString;
+        this.length = length(this.inputString);
+
+
+        // Remove double spaces and trailing/leading spaces from the input string
+        let workingString = this.inputString;
+        while(workingString.lastIndexOf("  ") > -1){
+            workingString = workingString.replace("  ", " ")
+        }
+        workingString = workingString.toLowerCase();
+
+    }
+    toString(){
+        return this.inputString;
+    }
+
+}
+
